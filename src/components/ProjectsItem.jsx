0ddd62d485d00a6a60f6f684c40f4f22a1834c73
Fred <fredsg22@hotmail.com>
2022-11-30 00:29:29 -0600

    Updated Projects Information

--- src/components/ProjectsItem.jsx
+++ src/components/ProjectsItem.jsx
@@ -9,19 +9,21 @@ export function ProjectsItem({
   stack,
 }) {
   return (
-    <div class="col-12 col-lg-4 col-md-6">
+    <div class="col-12 col-md-6 col-lg-6 col-xl-4">
       <article
         class="mx-auto card text-white bg-dark mb-3"
-        style={{ width: "18rem" }}
+        style={{ width: "21rem" }}
       >
-        <img class="card-img-top" src={imgUrl} alt="Card image cap" />
+        <a href={link} target="_blank">
+          <img class="card-img-top" src={imgUrl} alt="Card image cap" />
+        </a>
         <div class="card-body">
           <h5 class="card-title fw-bold">{title}</h5>
           <p class="card-text m-0">{description}</p>
           <div>
             {stack.map((item) => (
               <img
-                class="card-logos mt-1 mb-2 mx-1"
+                class="card-logos mt-2 mb-2 mx-1"
                 src={item}
                 alt="Skill Logo"
               />
@@ -30,7 +32,11 @@ export function ProjectsItem({
           <a href={link} class="btn btn-primary" target="_blank">
             <i class="fa-solid fa-arrow-up-right-from-square" /> Website
           </a>
-          <a href={codeLink} class="btn btn-secondary ms-2" target="_blank">
+          <a
+            href={codeLink}
+            class="btn btn-dark bg-gradient ms-2"
+            target="_blank"
+          >
             <i class="fa-brands fa-github" /> Code
           </a>
         </div>
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -3,7 +3,7 @@ export default [
     title: "Fé Media Website",
     imgUrl: "images/femediapreview.png",
     description:
-      "This project is based on my advertising startup, with it, I was able to enhance my WordPress skills, I created this website with the base editor from WordPress and without a layout template.",
+      "This project is based on my advertising startup, with it, I was able to improve my WordPress skills, I created this website with the base editor from WordPress and without a layout template. The main plugins I used were Greenshift and Gutenberg.",
     stack: ["images/wordpress-logo.svg"],
     link: "https://femedia.xyz/",
   },
@@ -11,7 +11,7 @@ export default [
     title: "React Shopping Cart",
     imgUrl: "images/ecommerce-website-preview.png",
     description:
-      "This is a working project that I will keep updating from time to time to make it a fully working e-commerce page based on React. ",
+      "This is a working project that I will keep updating from time to time to make it a fully working e-commerce page based on React. This project helped me fully understand React props, components, and how to use TypeScript rather than regular JavaScript.",
     stack: [
       "images/react-icon.png",
       "images/bootstrap-logo.svg",
@@ -23,7 +23,8 @@ export default [
   {
     title: "Old Portfolio Website",
     imgUrl: "images/oldportfoliopreview.png",
-    description: "This is my old website",
+    description:
+      "This was my first considerable project that allowed me to comprehend and sharpen my skills in CSS3, SASS, HTML5, and Bootstrap 4. The design for this page is mostly the same but the reasoning behind doing a new portfolio page was due to the chaos I did with the code back then, which lead to performance, maintainability, and re-usability issues. Therefore this portfolio is made with React props, components, and more.",
     stack: ["images/sass-logo.webp", "images/bootstrap-logo.svg"],
     link: "https://fredwilliams.netlify.app/",
     codeLink:
