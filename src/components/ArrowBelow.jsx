5c9f875bc08c09b0638721ca7e6252cf55c79863
Fred <fredsg22@hotmail.com>
2022-12-23 13:09:16 -0600

    Added smooth scroll behavior

--- src/components/ArrowBelow.jsx
+++ src/components/ArrowBelow.jsx
@@ -1,6 +1,9 @@
+import { useEffect } from "react";
+
 export function ArrowBelow() {
   return (
     <div
+      id="arrow-down"
       className="position-absolute heartbeat1 bottom-0 end-0
      mb-4 px-4 arrow-below"
     >
