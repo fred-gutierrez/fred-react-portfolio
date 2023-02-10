2e915fbe5752587e1f8003661363440076547f2e
Fred <fredsg22@hotmail.com>
2023-02-10 00:43:52 -0600

    made Skills props rather than copy and paste it on skills.jsx

---
+++ src/components/About/SkillsItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function SkillsItem(props) {
+  return (
+    <div className="col-6 col-lg-4">
+      <div className="d-flex justify-content-center">
+        {props.img ? (
+          <img
+            width="95px"
+            height="95px"
+            src={props.img}
+            alt={props.skillName}
+          />
+        ) : (
+          <>{props.svg}</>
+        )}
+      </div>
+      <p className="text-white text-center mt-2">{props.skillName}</p>
+    </div>
+  );
+}
--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -1,4 +1,4 @@
-// ------------------ <IMAGES> ------------------ //
+// Images
 
 import sassLogo from "/src/images/skills/sass-logo.webp";
 import typeScriptLogo from "/src/images/skills/typescript.svg";
@@ -10,15 +10,11 @@ import greenSockLogo from "/src/images/skills/gsap-greensock.svg";
 import gatsbyLogo from "/src/images/skills/gatsby-js-icon.svg";
 import postCSSLogo from "/src/images/skills/postcss-logo.png";
 
-// ------------------ </IMAGES> ------------------ //
-
-// ------------------ <PREVIEWS> ------------------ //
+// Previews
 
 import ecommercePreview from "/src/images/projects/ecommerce-website-preview.png";
 import feMediaPreview from "/src/images/projects/femediapreview.png";
 import tenziesGamePreview from "/src/images/projects/tenziesgamepreview.png";
-
-// ------------------ </PREVIEWS> ------------------ //
 
 const React = <span className="react-color fw-bold">React</span>;
 const WordPress = <span className="wordpress-color fw-bold">WordPress</span>;
