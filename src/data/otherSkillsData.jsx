cb9176dc2b5068d401e6f6d95af2f7950441c9bf
Fred <fredsg22@hotmail.com>
2023-08-26 22:55:05 -0600

    added style components and greensock to skills

--- src/data/otherSkillsData.jsx
+++ src/data/otherSkillsData.jsx
@@ -1,7 +1,19 @@
 import viteLogo from "/src/images/skills/tools/vite-logo.png";
 import gitLogo from "/src/images/skills/tools/git-logo.png";
+import greensockLogo from "/src/images/skills/tools/greensock-logo.png";
+import styledComponentsLogo from "/src/images/skills/tools/styled_c-logo.png";
 
 export default [
+  {
+    skillName: "Styled-Components",
+    img: styledComponentsLogo,
+    yearLearned: 2023,
+  },
+  {
+    skillName: "GreenSock",
+    img: greensockLogo,
+    yearLearned: 2022,
+  },
   {
     skillName: "Vite",
     img: viteLogo,
